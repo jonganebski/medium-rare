881f57d9849b7671f229816628e2fbb827415229
Jon Ganebski <jon.ganebski@gmail.com>
2020-12-04 09:45:13 +0900

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+require("dotenv").config();
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+
+const ENV = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "ts", "main.ts");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  mode: ENV,
+  plugins: [
+    new MiniCSSExtractPlugin({ filename: "styles.css" }),
+    new webpack.DefinePlugin({
+      "process.env": {},
+    }),
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader", exclude: "/node_modules" },
+      { test: /\.(ts)$/, use: "ts-loader", exclude: "/node_modules" },
+      {
+        test: /\.(scss)$/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  target: "web",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".scss"],
+    fallback: { fs: false, path: require.resolve("path-browserify") },
+  },
+  output: { path: OUTPUT_DIR, filename: "[name].js" },
+};
