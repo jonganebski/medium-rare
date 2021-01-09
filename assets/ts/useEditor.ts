c80a747b632fe7cfbeb124a713315a77ae469742
Jon Ganebski <jon.ganebski@gmail.com>
2021-01-09 13:52:29 +0900

    image delete

--- assets/ts/useEditor.ts
+++ assets/ts/useEditor.ts
@@ -1,13 +1,52 @@
-import EditorJS, { OutputBlockData } from "@editorjs/editorjs";
+import EditorJS, { OutputBlockData, OutputData } from "@editorjs/editorjs";
 import Axios from "axios";
 import { EDITORJS_CONFIG } from "./constants";
 import { publishBtn, saveStatusEl, unpublishBtn } from "./elements.header";
 import { overrideEditorJsStyleBody } from "./page.ReadStory";
 
-let timeoutId: NodeJS.Timeout;
+let timeoutId: any;
 let isCreated = false;
 let isSaved = false;
 let storyId = "";
+let imgHistory: string[] = [];
+
+const requestUnusedPhotosDelete = async (
+  savedData: OutputData
+): Promise<boolean> => {
+  const imgBlocks = savedData.blocks.filter((block) => block.type === "image");
+  try {
+    imgBlocks.forEach((imgBlock) => {
+      const usedImg = imgBlock.data.file.url;
+      imgHistory = imgHistory.filter((url) => url !== usedImg);
+    });
+    if (imgHistory.length === 0) {
+      return true;
+    }
+    const { status: removalStatus } = await Axios.delete("/api/photos", {
+      data: { images: Array.from(imgHistory) },
+    });
+    if (removalStatus < 300) {
+      imgHistory = [];
+      return true;
+    }
+    return false;
+  } catch {
+    return false;
+  }
+};
+
+const imageCollector = (holder: string) => {
+  const imgElements = document
+    .getElementById(holder)
+    ?.querySelectorAll(
+      ".image-tool__image-picture"
+    ) as NodeListOf<HTMLImageElement>;
+  imgElements?.forEach((imgEl) => {
+    if (!imgHistory.some((url) => url === imgEl.src)) {
+      imgHistory.push(imgEl.src);
+    }
+  });
+};
 
 export const handlePublishBtnClick = async (
   e: Event,
@@ -56,6 +95,7 @@ const saveAsDraft = async (editor: EditorJS) => {
     if (status < 300) {
       isSaved = true;
       saveStatusEl && (saveStatusEl.innerText = "Saved");
+      requestUnusedPhotosDelete(savedData);
       return;
     }
     throw new Error();
@@ -79,6 +119,7 @@ const createNewStory = async (editor: EditorJS) => {
       isCreated = true;
       isSaved = true;
       saveStatusEl && (saveStatusEl.innerText = "Saved");
+      requestUnusedPhotosDelete(savedData);
       return createdStoryId;
     }
     throw new Error();
@@ -140,16 +181,13 @@ export const useEditor = (
         childList: true,
         subtree: true,
       });
+    document.body.addEventListener("click", () => imageCollector(holder));
     publishBtn?.addEventListener("click", (e) =>
       handlePublishBtnClick(e, editor, 1)
     );
     unpublishBtn?.addEventListener("click", (e) => {
       handlePublishBtnClick(e, editor, -1);
     });
-    // window.addEventListener("beforeunload", () => {
-    //   clearTimeout(timeoutId);
-    //   !isSaved && saveAsDraft(editor);
-    // });
     overrideEditorJsStyleBody();
   });
 };
